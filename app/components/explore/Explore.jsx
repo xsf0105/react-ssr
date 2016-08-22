9a9c47c3e1747b20214957f1088000caa41ec4d5
allan91 <416175716@qq.com>
2016-08-22 23:10:10 +0800

    20160822/23:10 debug

--- app/components/explore/Explore.jsx
+++ app/components/explore/Explore.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
-import NavLink from '../lib/NavLink'
+import { Router, Route, browserHistory, IndexRoute ,Link} from 'react-router'
 
 import  './explore.css'
 const ACTIVE = { color: '#3E3D32' }
@@ -13,10 +12,10 @@ export default React.createClass({
         <h2>Welcome to Export</h2>
           <ul>
               <li>
-                  <NavLink activeStyle={ACTIVE} to="/ChildPage1">page1 tab(菜单一)</NavLink>
+                  <Link activeStyle={ACTIVE} to="/ChildPage1">page1 tab(菜单一)</Link>
               </li>
               <li>
-                  <NavLink activeStyle={ACTIVE} to="/ChildPage2">page2 tab(菜单二)</NavLink>
+                  <Link activeStyle={ACTIVE} to="/ChildPage2">page2 tab(菜单二)</Link>
               </li>
           </ul>
         {this.props.children}
