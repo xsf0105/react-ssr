fa6093d2100336ac66eadc6a206f1ca677ad0e83
xsf <416175716@qq.com>
2016-09-13 16:49:32 +0800

    remove dedux

--- app/components/news/News.jsx
+++ app/components/news/News.jsx
@@ -3,61 +3,6 @@ import { createStore, combineReducers } from 'redux'
 
 import  './news.scss'
 
-var userReducer = function (state = {}, action) {
-    console.log('state:', state, 'and action', action,111)
-
-    switch (action.type) {
-        case 'SET_NAME':
-            return {
-                ...state,
-                name: action.name
-            }
-        default:
-            return state;
-    }
-}
-var itemsReducer = function (state = [], action) {
-    console.log('itemsReducer was called with state', state, 'and action', action,222)
-
-    switch (action.type) {
-        case 'ADD_ITEM':
-            return [
-                ...state,
-                action.item
-            ]
-        default:
-            return state;
-    }
-}
-
-var reducer = combineReducers({
-    user: userReducer,
-    items: itemsReducer
-})
-var store_0 = createStore(reducer)
-
-console.log('store_0 state after initialization:', store_0.getState(),333)
-
-store_0.dispatch({
-    type: 'AN_ACTION'
-})
-
-var setNameActionCreator = function (name) {
-    return {
-        type: 'SET_NAME',
-        name: name
-    }
-}
-store_0.dispatch(setNameActionCreator('bob'))
-
-
-
-
-
-
-
-
-
 export default React.createClass({
   render() {
     return (
