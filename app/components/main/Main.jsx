8cd13ce107e3d3ca7649693370737254f5dddfc2
徐顺发 <416175716@qq.com>
2017-03-20 16:12:04 +0800

    debug

--- app/components/main/Main.jsx
+++ app/components/main/Main.jsx
@@ -5,7 +5,6 @@ export default React.createClass({
     return (
         <div>
           <h2 className="news">
-              Now,
             <p>You're in Main Page!</p>
           </h2>
         </div>
