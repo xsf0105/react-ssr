4f4068c32946fed982ba6e61dbfa06f4201d76c3
Allan <allan91coder@gmail.com>
2018-10-17 16:12:37 +0800

    Fix container change to controllers

--- server/routes/router.js
+++ server/routes/router.js
@@ -6,22 +6,22 @@ import Router from "koa-router";
 const router = new Router();
 
 // 首页路由
-router.get("/", require("../containers/index.js").index);
-router.post("/rest/v1/getList", require("../containers/index.js").getList);
+router.get("/", require("../controllers/index.js").index);
+router.post("/rest/v1/getList", require("../controllers/index.js").getList);
 
 // 登录页
-router.get("/login", require("../containers/login.js").index);
-router.post("/rest/v1/login", require("../containers/login.js").login);
-router.get("/logout", require("../containers/login.js").logout);
+router.get("/login", require("../controllers/login.js").index);
+router.post("/rest/v1/login", require("../controllers/login.js").login);
+router.get("/logout", require("../controllers/login.js").logout);
 
 // 注册页
-router.get("/reg", require("../containers/reg.js").index);
-router.post("/regUser", require("../containers/reg.js").reg);
-router.post("/vaildateUser", require("../containers/reg.js").vaildateUser);
-router.post("/vaildateEmail", require("../containers/reg.js").vaildateEmail);
+router.get("/reg", require("../controllers/reg.js").index);
+router.post("/regUser", require("../controllers/reg.js").reg);
+router.post("/vaildateUser", require("../controllers/reg.js").vaildateUser);
+router.post("/vaildateEmail", require("../controllers/reg.js").vaildateEmail);
 
 // 404
-router.get("/notFound", require("../containers/notFound.js").index);
+router.get("/notFound", require("../controllers/notFound.js").index);
 
 // set a router
 module.exports = router.routes();
